9d837ddcf8ac2c0c14969aff90df29d11481adf4
Smart309 <pattharapon_takham@cmu.ac.th>
2025-02-16 17:18:30 +0700

    Enhance AddAction component with form validation and additional fields for media, duration, and message templates

--- src/components/Modals/AddAction.tsx
+++ src/components/Modals/AddAction.tsx
@@ -1,4 +1,14 @@
-import { Box, Paper, TextField, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  FormControlLabel,
+  MenuItem,
+  Paper,
+  Switch,
+  TextField,
+  Typography,
+} from "@mui/material";
+import axios from "axios";
 import React, { useState } from "react";
 
 interface AddTriggerProps {
@@ -12,12 +22,26 @@ const AddAction: React.FC<AddTriggerProps> = ({ onClose }) => {
   };
 
   //Errors State
+  const validateForm = () => {
+    const newErrors = {
+      actionName: !actionName,
+      media: !media,
+      duration: !duration,
+      messageProblemTemplate: !messageProblemTemplate,
+      messageRecoveryTemplate: !messageRecoveryTemplate,
+    };
+
+    setErrors(newErrors);
+    return !Object.values(newErrors).some((error) => error);
+  };
+
   const [errors, setErrors] = useState({
     actionName: false,
+    media: false,
+    duration: false,
+    messageProblemTemplate: false,
+    messageRecoveryTemplate: false,
   });
-
-  //Action name
-  const [actionName, setActionName] = useState("");
 
   const textFieldProps = {
     size: "small" as const,
@@ -30,8 +54,90 @@ const AddAction: React.FC<AddTriggerProps> = ({ onClose }) => {
     },
   };
 
+  //Action name
+  const [actionName, setActionName] = useState("");
+
+  //Media field
+  const mediaOptions = [
+    { value: "all media", label: "All Media" },
+    { value: "email", label: "Email" },
+    { value: "line", label: "Line" },
+  ];
+  const [media, setMedia] = useState<string>(mediaOptions[0].value);
+  const handleMediaChange = (value: string) => {
+    setMedia(value);
+  };
+
+  //Duration field
+  const [duration, setDuration] = useState("30m");
+
+  //Message Problem Template
+  const [messageProblemTemplate, setMessageProblemTemplate] = useState("");
+
+  //Message Recovery Template
+  const [messageRecoveryTemplate, setMessageRecoveryTemplate] = useState("");
+
+  //Enabled field
+  const [enabled, setEnabled] = useState(true);
+
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    return;
+    e.preventDefault();
+
+    if (!validateForm()) {
+      alert("Please fill in all required fields");
+      return;
+    }
+
+    const success = await StoreNewAction(
+      actionName,
+      media,
+      duration,
+      messageProblemTemplate,
+      messageRecoveryTemplate,
+      enabled
+    );
+
+    if (success) {
+      setActionName("");
+      setMedia(mediaOptions[0].value);
+      setDuration("");
+      setMessageProblemTemplate("");
+      setMessageRecoveryTemplate("");
+      setEnabled(true);
+      onClose();
+    } else {
+      alert("Failed to add action. Please try again.");
+    }
+  };
+
+  //user
+  const user_id = localStorage.getItem("user_id");
+
+  const StoreNewAction = async (
+    actionName: string,
+    media: string,
+    duration: string,
+    messageProblemTemplate: string,
+    messageRecoveryTemplate: string,
+    enabled: boolean
+  ): Promise<boolean> => {
+    try {
+      const requestBody = {
+        action_name: actionName,
+        user_id: user_id,
+        media,
+        duration,
+        messageProblemTemplate,
+        messageRecoveryTemplate,
+        enabled,
+      };
+
+      await axios.post("http://127.0.0.1:3000/action", requestBody);
+      return true;
+    } catch (error) {
+      console.error("Error recording new action:", error);
+      return false;
+    }
   };
 
   return (
@@ -59,6 +165,198 @@ const AddAction: React.FC<AddTriggerProps> = ({ onClose }) => {
               error={errors.actionName}
               helperText={errors.actionName ? "Action name is required" : ""}
             />
+          </Box>
+
+          {/* Media field */}
+          <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
+            <Box sx={{ display: "flex", alignItems: "center", minWidth: 150 }}>
+              <Typography color="error" {...typographyProps}>
+                *
+              </Typography>
+              <Typography sx={{ ml: 1 }} {...typographyProps}>
+                Media
+              </Typography>
+            </Box>
+            <TextField
+              select
+              value={media}
+              onChange={(e) => handleMediaChange(e.target.value)}
+              error={errors.media}
+              helperText={errors.media ? "Media is required" : ""}
+              sx={{ width: "11%" }}
+            >
+              {mediaOptions.map((option) => (
+                <MenuItem key={option.value} value={option.value}>
+                  {option.label}
+                </MenuItem>
+              ))}
+            </TextField>
+          </Box>
+
+          {/* Duration */}
+          <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
+            <Box sx={{ display: "flex", alignItems: "center", minWidth: 150 }}>
+              <Typography color="error" {...typographyProps}>
+                *
+              </Typography>
+              <Typography sx={{ ml: 1 }} {...typographyProps}>
+                Duration
+              </Typography>
+            </Box>
+            <TextField
+              sx={{ width: "11%" }}
+              value={duration}
+              onChange={(e) => setDuration(e.target.value)}
+              error={errors.duration}
+              helperText={errors.duration ? "Duration is required." : ""}
+            />
+          </Box>
+
+          {/* Message Problem Template */}
+          <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
+            <Box sx={{ display: "flex", alignItems: "center", minWidth: 150 }}>
+              <Typography color="error" {...typographyProps}>
+                *
+              </Typography>
+              <Typography sx={{ ml: 1 }} {...typographyProps}>
+                Message Problem Template
+              </Typography>
+            </Box>
+            <TextField
+              {...textFieldProps}
+              value={messageProblemTemplate}
+              onChange={(e) => setMessageProblemTemplate(e.target.value)}
+              multiline
+              rows={4}
+              sx={{
+                ...textFieldProps.sx,
+                "& .MuiOutlinedInput-root": {
+                  padding: "8px",
+                },
+              }}
+              error={errors.messageProblemTemplate}
+              helperText={
+                errors.messageProblemTemplate
+                  ? "Message problem template is required."
+                  : ""
+              }
+            />
+          </Box>
+
+          {/* Message Recovery Template */}
+          <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
+            <Box sx={{ display: "flex", alignItems: "center", minWidth: 150 }}>
+              <Typography color="error" {...typographyProps}>
+                *
+              </Typography>
+              <Typography sx={{ ml: 1 }} {...typographyProps}>
+                Message Recovery Template
+              </Typography>
+            </Box>
+            <TextField
+              {...textFieldProps}
+              value={messageRecoveryTemplate}
+              onChange={(e) => setMessageRecoveryTemplate(e.target.value)}
+              multiline
+              rows={4}
+              sx={{
+                ...textFieldProps.sx,
+                "& .MuiOutlinedInput-root": {
+                  padding: "8px",
+                },
+              }}
+              error={errors.messageRecoveryTemplate}
+              helperText={
+                errors.messageRecoveryTemplate
+                  ? "Message recovery template is required."
+                  : ""
+              }
+            />
+          </Box>
+
+          {/* Enabled */}
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
+          >
+            {/* Enabled Switch */}
+            <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
+              <Box
+                sx={{ display: "flex", alignItems: "center", minWidth: 150 }}
+              >
+                <Typography color="error" {...typographyProps}>
+                  *
+                </Typography>
+                <Typography sx={{ ml: 1 }} {...typographyProps}>
+                  Enabled
+                </Typography>
+              </Box>
+              <FormControlLabel
+                control={
+                  <Switch
+                    checked={enabled}
+                    onChange={(e) => setEnabled(e.target.checked)}
+                    sx={{
+                      "& .MuiSwitch-switchBase.Mui-checked": {
+                        color: "#4CAF50",
+                        "&:hover": {
+                          backgroundColor: "#4CAF5022",
+                        },
+                      },
+                      "& .MuiSwitch-switchBase.Mui-checked + .MuiSwitch-track":
+                        {
+                          backgroundColor: "#4CAF50",
+                        },
+                      "& .MuiSwitch-switchBase.Mui-disabled": {
+                        color: "#bdbdbd",
+                      },
+                      "& .MuiSwitch-switchBase.Mui-disabled + .MuiSwitch-track":
+                        {
+                          backgroundColor: "#e0e0e0",
+                        },
+                    }}
+                  />
+                }
+                label=""
+              />
+            </Box>
+
+            {/* Button section */}
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "flex-end",
+                gap: 2,
+                mt: 2,
+              }}
+            >
+              <Button
+                variant="outlined"
+                color="error"
+                onClick={onClose}
+                sx={{ fontSize: 14 }}
+              >
+                Cancel
+              </Button>
+              <Button
+                type="submit"
+                variant="outlined"
+                sx={{
+                  fontSize: 14,
+                  color: "black",
+                  borderColor: "black",
+                  "&:hover": {
+                    color: "red",
+                    borderColor: "red",
+                  },
+                }}
+              >
+                Add
+              </Button>
+            </Box>
           </Box>
         </Box>
       </Paper>
