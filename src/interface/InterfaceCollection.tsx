453d73322a072aecfa0df622c0eb3b35d49fcecb
Smart309 <pattharapon_takham@cmu.ac.th>
2025-02-25 21:38:04 +0700

    Enhance InterfaceCollection by adding trigger and item templates for improved data structure

--- src/interface/InterfaceCollection.tsx
+++ src/interface/InterfaceCollection.tsx
@@ -116,5 +116,42 @@ export interface ITemplate {
   _id: string;
   template_name: string;
   description: string;
-  items: Item[];
+  items: ItemTemplate[];
+  triggers: ITriggerTemplate[];
+}
+
+export interface ITriggerTemplate {
+  trigger_name: string;
+  severity: string;
+  expression: string;
+  ok_event_generation: string;
+  recovery_expression: string;
+  expressionPart: ExpressionPart[];
+  expressionRecoveryPart: RecoveryPart[];
+}
+
+export interface ItemTemplate {
+  item_name: string;
+  oid: string;
+  type: string;
+  unit: string;
+  interval: number;
+}
+
+export interface ExpressionPart {
+  item: string;
+  operation: string;
+  value: string;
+  operator?: string; // 'and' or 'or'
+  functionofItem: string;
+  duration: string;
+}
+
+export interface RecoveryPart {
+  item: string;
+  operation: string;
+  value: string;
+  operator?: string; // 'and' or 'or'
+  functionofItem: string;
+  duration: string;
 }
