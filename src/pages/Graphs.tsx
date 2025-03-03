99c56ec3b831c9e82268e57e91c959a48175ee8e
Smart309 <pattharapon_takham@cmu.ac.th>
2025-03-03 23:43:16 +0700

    Add selectedDateTimeEnd dependency and disable future dates in DateTimePicker components

--- src/pages/Graphs.tsx
+++ src/pages/Graphs.tsx
@@ -311,7 +311,7 @@ const Graphs: React.FC = () => {
     };
 
     setLastTime();
-  }, [selectedLastTime]);
+  }, [selectedLastTime, selectedDateTimeEnd]);
 
   //Apply Button
   const handleApplyClick = () => {
@@ -728,6 +728,8 @@ const Graphs: React.FC = () => {
                   setIsAuto(false);
                 }
               }}
+              disableFuture
+              maxDateTime={selectedDateTimeEnd}
             ></DateTimePicker>
             <DateTimePicker
               label="End"
@@ -741,6 +743,7 @@ const Graphs: React.FC = () => {
                   setIsAuto(false);
                 }
               }}
+              disableFuture
             ></DateTimePicker>
             {/* Cheack Box For Auto */}
             <Box sx={{ ml: 1 }}>
